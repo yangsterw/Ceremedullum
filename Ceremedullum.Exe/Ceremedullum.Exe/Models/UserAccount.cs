3e38073c9cdb7c10d113ac6d011d00a94b6615b8
Yang Wang <yang@yangwang.org>
2019-10-20 18:52:34 -0700

    Added MVVM pattern and user account class
    User account and MVVM pattern added.

---
+++ Ceremedullum.Exe/Ceremedullum.Exe/Models/UserAccount.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ceremedullum.Exe.Models
+{
+    public class UserAccount
+    {
+        string UserName {  get; set; }
+        string Password {  get; set; }
+        string Token {  get; set; }
+    }
+}
