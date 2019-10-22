8ac0e7053a8f11b06f352f9e13f0400aba210e98
Yang Wang <yang@yangwang.org>
2019-10-21 22:35:38 -0700

    Added login UI and UserAccount class
    UserAccount = Singleton implementation.
    
    UI for login has been added and fixed.

--- Ceremedullum.Exe/Ceremedullum.Exe/Models/UserAccount.cs
+++ Ceremedullum.Exe/Ceremedullum.Exe/Models/UserAccount.cs
@@ -6,10 +6,29 @@ using System.Threading.Tasks;
 
 namespace Ceremedullum.Exe.Models
 {
-    public class UserAccount
+    public sealed class UserAccount
     {
-        string UserName {  get; set; }
-        string Password {  get; set; }
-        string Token {  get; set; }
+        private static readonly Lazy<UserAccount>
+            Lazy =
+                new Lazy<UserAccount>
+                    (() => new UserAccount());
+
+        public static UserAccount Instance => Lazy.Value;
+
+        private string UserName { get; set; }
+        private string Password { get; set; }
+        private string Token { get; set; }
+
+        public UserAccount()
+        {
+
+        }
+
+        public void SetUser(string username, string password)
+        {
+           this.UserName = username;
+           this.Password = password;
+        }
     }
+}
 }
